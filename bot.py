07140417ef276fd0a551b82eff134c389832037f
sandeepdcdc <hmis_3@dcdc.co.in>
2026-05-05 17:18:11 +0530

    Base URL update from local to render url

--- bot.py
+++ bot.py
@@ -559,7 +559,8 @@ import requests
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-BASE_URL = "http://127.0.0.1:5000"
+# BASE_URL = "http://127.0.0.1:5000"
+BASE_URL = "https://telegram-dialysis-chatbot.onrender.com"
 API_KEY = os.getenv("API_KEY")
 
 HEADERS = {"x-api-key": API_KEY}
